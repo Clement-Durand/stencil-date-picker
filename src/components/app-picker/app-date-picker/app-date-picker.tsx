99887ee06d978aaf531c22fe5c6a2d85b34e1b61
Clement-Durand <clement.durand@etu.unice.fr>
2019-02-12 11:11:04 +0100

    Added some style and data
    Clicking on selected date now opens the pickers and clicking around closes them

--- src/components/app-picker/app-date-picker/app-date-picker.tsx
+++ src/components/app-picker/app-date-picker/app-date-picker.tsx
@@ -7,6 +7,11 @@ import {Component} from '@stencil/core';
 export class AppDatePicker {
 
   render() {
+    const current_date = new Date();
+    const current_year = current_date.getFullYear();
+    //TODO prettify
+    const current_month = current_date.getMonth() < 10 ? '0' + current_date.getMonth() : current_date.getMonth();
+    const current_day = current_date.getDay() < 10 ? '0' + current_date.getDay() : current_date.getDay();
     const days = {
       'Su': 'S',
       'Mo': 'M',
@@ -15,6 +20,21 @@ export class AppDatePicker {
       'Th': 'T',
       'Fr': 'F',
       'Sa': 'S',
+    };
+
+    const months = {
+      'January': 31,
+      'February': 28,
+      'March': 31,
+      'April': 30,
+      'May': 31,
+      'June': 30,
+      'July': 31,
+      'August': 31,
+      'September': 30,
+      'October': 31,
+      'November': 30,
+      'December': 31,
     };
     const numbers = [1,2,3];
 
@@ -33,13 +53,24 @@ export class AppDatePicker {
         }
       </div>,
       <div>
+        {current_day} / {current_month} / {current_year}
+      </div>,
+      <div>
         {
           Object.keys(days).map(day => {
             return (
-              <th>
-                <h5>{day}</h5>
-                <h5>{days[day]}</h5>
-              </th>
+              <div class="day-name">
+                <span>{days[day]}</span>
+              </div>
+            )
+          })
+        }
+        {
+          Object.keys(months).map(month => {
+            return (
+              <div class="day-name">
+                <span>{months[month]}</span>
+              </div>
             )
           })
         }
--- src/components/app-picker/app-picker.tsx
+++ src/components/app-picker/app-picker.tsx
@@ -7,14 +7,17 @@
 export class AppPicker {
   isPicking = false;
 
-  showPickers(id) {
+  showPickers() {
     if(this.isPicking) {
-      document.getElementById(id).style.display = 'none';
+      document.getElementById('pickers').style.display = 'none';
+      document.getElementById('date-btn').className = 'date-btn';
     } else {
-      document.getElementById(id).style.display = 'grid';
+      document.getElementById('pickers').style.display = 'grid';
+      document.getElementById('date-btn').className += ' active';
     }
     this.isPicking = !this.isPicking;
   }
+
 
   render() {
     return [
@@ -24,11 +27,12 @@ export class AppPicker {
         </div>
         <div class="dates">
           <span>Current Date - D DD / MM / YYYY - HH:MM</span>
-          <span>Selected Date - <button class="date-btn" onClick={()=> this.showPickers('pickers')}>D DD / MM / YYYY - HH:MM</button></span>
+          <span>Selected Date - <button id="date-btn" class="date-btn" onClick={()=> this.showPickers()}>D DD / MM / YYYY - HH:MM</button></span>
         </div>
-        <div id="pickers" class="pickers">
-          <app-date-picker></app-date-picker>
-          <app-time-picker></app-time-picker>
+        {/* TODO find a way to make onClick cleaner */}
+        <div id="pickers" class="pickers" onClick={()=> this.showPickers()}>
+          <app-date-picker onClick={() => this.showPickers()}></app-date-picker>
+          <app-time-picker onClick={() => this.showPickers()}></app-time-picker>
         </div>
 
       </ion-content>
